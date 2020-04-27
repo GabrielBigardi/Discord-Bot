8a22152f617c76a0e998ae55640d5d4959775f27
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-04-27 16:03:24 -0300

    Create currentonline.js

---
+++ commands/info/currentonline.js
@@ -0,0 +1,14 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const https = require('https');
+
+module.exports = {
+    name: "onlineagora",
+    aliases: ["currentlyonline"],
+    category: "info",
+    description: "Verifica quantos usuários e servidores está escutando.",
+    usage: "[comando | alias]",
+    run: async (client, message, args) => {
+        message.channel.send(`Atualmente escutando ${client.users.size} pessoas em ${client.guilds.size} servidores.`);
+    }
+}
