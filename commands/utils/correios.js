d7b02956ff3ff4d6db6cfe10015d0058e4033283
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-07-12 18:48:07 -0300

    Update correios.js

--- commands/utils/correios.js
+++ commands/utils/correios.js
@@ -17,31 +17,23 @@ module.exports = {
         let URL = `https://correios.postmon.com.br/webservice/buscaEventos/?objetos=${args[0]}`;
 		
 		https.get(URL, (resp) => {
-                        let data = '';
-                
-                        // A chunk of data has been recieved.
-                        resp.on('data', (chunk) => {
-                            data += chunk;
-                        });
-                
-                        // The whole response has been received. Print out the result.
-                        resp.on('end', () => {
-							
-                            //var itemName = decodeURI(element);
-                            //var volume = JSON.parse(data).volume;
-                            //var precoMenor = JSON.parse(data).lowest_price;
-                            //var precoMedio = JSON.parse(data).median_price;
-        
-                            const embed = new RichEmbed()
-                            .setColor('#0000ff')
-                            .addField('Informação da encomenda', stripIndents`
-                                **Data:** ${data}`, true)
-        
-                                message.channel.send(embed);
-                        });
-                
-                        }).on("error", (err) => {
-                            console.log("Error: " + err.message);
-                        });
-		}
+			let data = '';
+			
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+			
+			resp.on('end', () => {
+				const embed = new RichEmbed().setColor('#0000ff')
+					.addField('Informação da encomenda', stripIndents`**Data:** ${data}`, true)
+					message.channel.send(embed);
+			});
+			
+			
+			
+		}).on("error", (err) => {
+			console.log("Error: " + err.message);
+        });
+		
+	}
 }
