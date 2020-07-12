8903f95cb9d95f9b9114dbd8b5a8f137b25e5329
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-07-12 19:16:06 -0300

    Add

--- commands/utils/correios.js
+++ commands/utils/correios.js
@@ -1,7 +1,7 @@
 const { getMember, formatDate } = require("../../functions.js");
 const { RichEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
-const https = require('https');
+const TrackingCorreios = require('tracking-correios');
 
 
 module.exports = {
@@ -11,43 +11,8 @@ module.exports = {
     description: "Retorna informações de um pacote pelos correios",
     usage: "[comando | alias] <código de rastreio>",
     run: async (client, message, args) => {
-		
 		if(!args[0]) return;
-		
-        let URL = `https://correios.postmon.com.br/webservice/buscaEventos/?objetos=${args[0]}`;
-		
-		https.get(URL, (resp) => {
-			let data = '';
-			
-			resp.on('data', (chunk) => {
-				data += chunk;
-			});
-			
-			resp.on('end', () => {
-				var dataJson = JSON.parse(data);
-				var dataEvento = dataJson.objeto[0].evento[0];
-				var dataCidade = dataJson.cidade;
-				var dataUF = dataJson.uf;
-				var dataDesc = dataJson.descricao;
-				var dataData = dataJson.data;
-				
-				
-				
-				const embed = new RichEmbed()
-                            .setColor('#0000ff')
-                            .addField('Informação da Skin', stripIndents`
-                                **Local:** ${dataCidade} - ${dataUF}
-                                **Descrição:** ${dataDesc}
-                                **Data:** ${dataData}`, true)
-        
-                message.channel.send(embed);
-			});
-			
-			
-			
-		}).on("error", (err) => {
-			console.log("Error: " + err.message);
-        });
-		
+
+		TrackingCorreios.track(args[0]).then(console.log)
 	}
 }
