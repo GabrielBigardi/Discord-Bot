0bd368bad197fd24927a495b545eae4ef0df333e
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-07-12 19:19:31 -0300

    Update correios.js

--- commands/utils/correios.js
+++ commands/utils/correios.js
@@ -13,6 +13,6 @@ module.exports = {
     run: async (client, message, args) => {
 		if(!args[0]) return;
 
-		TrackingCorreios.track('SB231363632BR').then(console.log)
+		TrackingCorreios.track('OK204454682BR').then(console.log)
 	}
 }
