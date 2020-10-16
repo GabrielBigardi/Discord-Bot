15ea86d5a6e88e6caee488ec2d6f9945cfcf0523
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-10-15 22:16:47 -0300

    Update gamersclub.js

--- commands/info/gamersclub.js
+++ commands/info/gamersclub.js
@@ -30,8 +30,8 @@ module.exports = {
         
             // The whole response has been received. Print out the result.
             resp.on('end', () => {
+				console.log(data);
                 var teste = JSON.parse(data);
-				console.log(teste);
             });
         
         }).on("error", (err) => {
