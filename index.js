d14cb5e71e3a76502b251d0362a502b843a4b03f
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-12-08 12:47:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,28 +42,23 @@ client.on('guildDelete', guild => {
 
 client.on('guildMemberAdd', async member => {
 	console.log(colors.green.bold(`Um membro foi adicionado: ${member.user.username}`));
-	let canal = client.channels.cache.get('691183263204507679');
-	let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
-    let mask = await jimp.read('projetojimp/mascara.png');
-	let fundo = await jimp.read('projetojimp/fundo.png');
-
-	//member.roles.add(member.guild.roles.find(role => role.name === "🎮│Membros")).catch(() => {
-	//	console.log("Erro ao tentar adicionar regra não existente a usuário.");
-	//});
-
-	jimp.read(member.user.displayAvatarURL())
-        .then(avatar => {
-            avatar.resize(130, 130);
-            mask.resize(130, 130);
-            avatar.mask(mask);
-            fundo.print(fonte, 170, 175, member.user.username);
-            fundo.composite(avatar, 22, 90).write('bemvindo.png');
-            canal.send(``, { files: ["bemvindo.png"] });
-            //console.log('Imagem enviada para o Discord: ' + member.user.displayAvatarURL());
-        })
-        .catch(err => {
-            console.log('Erro ao carregar imagem: ' + err);
-        });
+	//let canal = client.channels.cache.get('691183263204507679');
+	//let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+    //let mask = await jimp.read('projetojimp/mascara.png');
+	//let fundo = await jimp.read('projetojimp/fundo.png');
+    //
+	//jimp.read(member.user.displayAvatarURL())
+    //    .then(avatar => {
+    //        avatar.resize(130, 130);
+    //        mask.resize(130, 130);
+    //        avatar.mask(mask);
+    //        fundo.print(fonte, 170, 175, member.user.username);
+    //        fundo.composite(avatar, 22, 90).write('bemvindo.png');
+    //        canal.send(``, { files: ["bemvindo.png"] });
+    //    })
+    //    .catch(err => {
+    //        console.log('Erro ao carregar imagem: ' + err);
+    //    });
 });
 
 client.on('message', async message => {
