957aaed4fd4360060e93abea3a6c32da9d9b65f9
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2021-10-08 20:38:59 -0300

    Update points.js

--- commands/info/points.js
+++ commands/info/points.js
@@ -7,47 +7,94 @@ module.exports = {
     aliases: ["points"],
     category: "info",
     description: "Checa pontos de determinado usuário.",
-    usage: "[comando | alias] <usuário>",
+    usage: "[comando | alias] <usuário> <canal>",
     run: async (client, message, args) => {
         if(!args[0]) return;
-
-        https.get(`https://api.streamelements.com/kappa/v2/points/5b64234fe8fd184d761eadcf/${args[0]}`, (resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        resp.on('end', () => {
-            var parsedJson = JSON.parse(data);
-            var pontos = parsedJson.points;
-            var pontosTotais = parsedJson.pointsAlltime;
-            var assistido = parsedJson.watchtime;
-            var rank = parsedJson.rank;
-
-            
-        
-            if(pontos){
-                const embed = new MessageEmbed()
-                            .setColor('#00FF00')
-                            .addField('Informação StreamElements', stripIndents`
-                                **Nome:** ${args[0]}
-                                **Pontos:** ${pontos}
-                                **Pontos totais farmados:** ${pontosTotais}
-                                **Tempo assistido:** ${assistido}
-                                **Rank:** ${rank}`, true);
-                message.channel.send(embed);
-            }else{
-                const embed = new MessageEmbed()
-                            .setColor('#FF0000')
-                            .addField('Erro ao executar comando', stripIndents`
-                                **USUÁRIO NÃO ENCONTRADO !**`, true);
-                message.channel.send(embed);
-            }
-
-        });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
+		if(!args[1]) return;
+		
+		GetChannelData(args[1]).then(channelData => {
+			//console.log(channelData);
+			GetFarmStatus(channelData, args[0]).then(farmData => {
+				//console.log(farmData)
+				const embed = new MessageEmbed()
+					.setColor('#00FF00')
+					.addField('Informação StreamElements', stripIndents`
+						**Usuário:** ${args[0]}
+						**Canal:** ${channelData.displayName}
+						**Pontos:** ${farmData.points}
+						**Pontos totais farmados:** ${farmData.pointsAlltime}
+						**Tempo assistido:** ${farmData.watchtime}
+						**Rank:** ${farmData.rank}`, true);
+				message.channel.send(embed);
+			}).catch(error => {
+				console.log(error);
+			});
+		}).catch(error => {
+			console.log(error);
+		})
+//var parsedJson = JSON.parse(data);
+//var pontos = parsedJson.points;
+//var pontosTotais = parsedJson.pointsAlltime;
+//var assistido = parsedJson.watchtime;
+//var rank = parsedJson.rank;
+//if(pontos){
+//
+//}else{
+//	const embed = new MessageEmbed()
+//				.setColor('#FF0000')
+//				.addField('Erro ao executar comando', stripIndents`
+//					**USUÁRIO NÃO ENCONTRADO !**`, true);
+//	message.channel.send(embed);
+//}
     }
+}
+
+function GetChannelData(channelName)
+{
+	//https://api.streamelements.com/kappa/v2/channels/murilo_rt
+	return new Promise((resolve, reject) => {
+		https.get(`https://api.streamelements.com/kappa/v2/channels/${channelName}`, (resp) => {
+			let data = '';
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+		
+			resp.on('end', () => {
+				try{
+					const parsedData = JSON.parse(data);
+					resolve(parsedData);
+				}catch(e){
+					reject(e.message);
+				}
+				
+			});
+		
+		}).on("error", (e) => {
+			reject(e.message);
+		});
+	});
+}
+
+function GetFarmStatus(channelData, username)
+{
+	return new Promise((resolve, reject) => {
+		https.get(`https://api.streamelements.com/kappa/v2/points/${channelData._id}/${username}`, (resp) => {
+			let data = '';
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+		
+			resp.on('end', () => {
+				try{
+					const parsedData = JSON.parse(data);
+					resolve(parsedData);
+				}catch(e){
+					reject(e.message);
+				}
+			});
+	
+		}).on("error", (err) => {
+			reject(e.message);
+		});
+	});
 }
